c9e030df00ab6e712dc1706060c814d45e6f437f
janteque2 <31700466+janteque2@users.noreply.github.com>
2017-09-08 13:55:32 -0500

    Update example_get.php

--- examples/example_get.php
+++ examples/example_get.php
@@ -2,13 +2,13 @@
 require '../Meli/meli.php';
 require '../configApp.php';
 
-$meli = new Meli($appId, $secretKey);
+$meli = new Meli($5384720588720865, $K0UFRuhX33UE2Z20wHCiSYUMembRphD4);
 
 $params = array();
 
 $url = '/sites/' . $siteId;
 
-$result = $meli->get($url, $params);
+$result = $meli->post($url, $params);
 
 echo '<pre>';
 print_r($result);
